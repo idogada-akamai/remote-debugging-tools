978a75f63311ee23f1854620cc8dbf149bf5f519
Ido Gada <igada@akamai.com>
2023-03-16 01:44:09 +0200

    Use import to simplify project configuration

--- context-selector.sh
+++ context-selector.sh
@@ -50,11 +50,9 @@ get_deployment_label_selector() {
     context=$1
     namespace=$2
     deployment=$3
-    label_name=$4
+    selector_label=$4
 
-    json_path=".spec.selector.matchLabels"
-    # command="kubectl --context=$context --namespace=$namespace get deploy -o json | jq -r "$json_path""
-    # echo $command
+    json_path=".spec.selector.matchLabels.$selector_label"
     selector=$(kubectl --context=$context --namespace=$namespace get deploy $deployment -o json | jq -r "$json_path")
 
     echo $selector
@@ -67,11 +65,14 @@ start_debugging_session() {
 }
 
 namespace="app"
+selector_label="service"
 
 context=$(choose_context_interactive)
 repo=$(get_repo_name)
 deployment=$(get_repo_deployment $context $namespace $repo)
 echo "Deployment $deployment is matching your repo"
 
-selector=$(get_deployment_label_selector $context $namespace $deployment)
+selector=$(get_deployment_label_selector $context $namespace $deployment $selector_label)
 echo "You deployment selector is: $selector"
+
+devspace print --var SERVICE=$selector
